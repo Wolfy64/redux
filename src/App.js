3790686f0dee146ee3ab3630e4b85f5bf645a59f
David <ledavid64@gmail.com>
2018-08-13 12:11:53 -0700

    Connecting the Store to React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
-import { createStore } from 'redux';
-import reducer from './store/reducer';
 import Counter from './containers/Counter/Counter';
 import './App.css';
-
-const store = createStore(reducer);
 
 class App extends Component {
   render() {
