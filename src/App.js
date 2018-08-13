4b8ab09d8c999b5497cbdd979b2c95af2f89ef64
David <ledavid64@gmail.com>
2018-08-13 11:42:41 -0700

    Connecting React to Redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-
+import { createStore } from 'redux';
+import reducer from './store/reducer';
 import Counter from './containers/Counter/Counter';
 import './App.css';
+
+const store = createStore(reducer);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Counter />
+        <Counter />
       </div>
     );
   }
