5024c319222f126a472c4d45e10a7da035767027
David <ledavid64@gmail.com>
2018-08-13 11:13:44 -0700

    Dispatching Actions

--- redux-basic.js
+++ redux-basic.js
@@ -7,6 +7,18 @@ const initaialState = {
 
 // Reducer
 const rootReducer = (state = initaialState, action) => {
+  if (action.type === 'INC_COUNTER') {
+    return {
+      ...state,
+      counter: state.counter + 1
+    };
+  }
+  if (action.type === 'ADD_COUNTER') {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    };
+  }
   return state;
 };
 
@@ -15,5 +27,8 @@ const store = createStore(rootReducer);
 console.log('[getState]', store.getState());
 
 // Dispatching Action
+store.dispatch({ type: 'INC_COUNTER' });
+store.dispatch({ type: 'ADD_COUNTER', value: 10 });
+console.log('[getState]', store.getState());
 
 // Subscription
