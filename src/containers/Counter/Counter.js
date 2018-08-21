5d4c1145b3587fdb5d1d218c0e16f2a3b334bc48
David <ledavid64@gmail.com>
2018-08-21 11:42:17 -0700

    Outsourcing Action Types

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import * as actionType from '../../store/actions';
 
 class Counter extends Component {
   render() {
@@ -45,12 +46,12 @@ const mapToStateToProps = state => {
 
 const mapDispatchToProps = () => dispatch => {
   return {
-    onIncrementCounter: () => dispatch({ type: 'INCREMENT' }),
-    onDecrementCounter: () => dispatch({ type: 'DECREMENT' }),
-    onAddCounter: () => dispatch({ type: 'ADD', value: 5 }),
-    onSubtractCounter: () => dispatch({ type: 'SUBTRACT', value: 5 }),
-    onStoreResults: () => dispatch({ type: 'STORE_RESULT' }),
-    onDeleteResult: (id) => dispatch({ type: 'DELETE_RESULT', resultElId: id })
+    onIncrementCounter: () => dispatch({ type: actionType.INCREMENT }),
+    onDecrementCounter: () => dispatch({ type: actionType.DECREMENT }),
+    onAddCounter: () => dispatch({ type: actionType.ADD, value: 5 }),
+    onSubtractCounter: () => dispatch({ type: actionType.SUBTRACT, value: 5 }),
+    onStoreResults: () => dispatch({ type: actionType.STORE_RESULT }),
+    onDeleteResult: (id) => dispatch({ type: actionType.DELETE_RESULT, resultElId: id })
   };
 };
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,5 @@
+import * as actionType from './actions'
+
 const initialState = {
   counter: 0,
   results: []
@@ -5,20 +7,20 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'INCREMENT':
+    case actionType.INCREMENT:
       return { ...state, counter: state.counter + 1 };
-    case 'DECREMENT':
+    case actionType.DECREMENT:
       return { ...state, counter: state.counter - 1 };
-    case 'ADD':
+    case actionType.ADD:
       return { ...state, counter: state.counter + action.value };
-    case 'SUBTRACT':
+    case actionType.SUBTRACT:
       return { ...state, counter: state.counter - action.value };
-    case 'STORE_RESULT':
+    case actionType.STORE_RESULT:
       return {
         ...state,
         results: state.results.concat({ id: new Date(), value: state.counter })
       };
-    case 'DELETE_RESULT':
+    case actionType.DELETE_RESULT:
       // const id = 2;
       // const newArray = [...state.results];
       // newArray.splice(id, 1);
